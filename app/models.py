fc776307a4f7523183919a939fdcb634c70e7f0a
tr0ublekat <kirillkojev8@gmail.com>
2025-08-29 14:58:24 +0300

    add models for sql orm

---
+++ app/models.py
@@ -0,0 +1,25 @@
+import enum
+
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql import func
+
+Base = declarative_base()
+
+
+# Статусы задач
+class TaskStatus(str, enum.Enum):
+    CREATED = "created"
+    IN_PROGRESS = "in_progress"
+    COMPLETED = "completed"
+
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String(100), nullable=False)
+    description = Column(Text, nullable=True)
+    status = Column(String(20), default=TaskStatus.CREATED.value, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
