0d07daa199b5219b76c7aa8ed36785c0eaf107f3
tr0ublekat <kirillkojev8@gmail.com>
2025-08-29 16:06:02 +0300

    add aiosqlite and main app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
-def main():
-    print("Hello from task-manager!")
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from . import database, models
+from .routers import router as task_router
 
 
-if __name__ == "__main__":
-    main()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan для создания таблиц при старте приложения."""
+
+    async with database.engine.begin() as conn:
+        await conn.run_sync(models.Base.metadata.create_all)
+    yield
+
+
+app = FastAPI(
+    title="Тестовое задание для skycapital.group", version="1.0.0", lifespan=lifespan
+)
+
+app.include_router(task_router)
