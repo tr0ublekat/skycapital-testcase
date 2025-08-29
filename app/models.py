39d40521f7c2b9ba4c8d2e6602b074263eace482
tr0ublekat <kirillkojev8@gmail.com>
2025-08-29 16:20:23 +0300

    fix default value for "updated_at" column

--- app/models.py
+++ app/models.py
@@ -21,5 +21,7 @@ class Task(Base):
     title = Column(String(100), nullable=False)
     description = Column(Text, nullable=True)
     status = Column(String(20), default=TaskStatus.CREATED.value, nullable=False)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    created_at = Column(DateTime(timezone=True), default=func.now())
+    updated_at = Column(
+        DateTime(timezone=True), default=func.now(), onupdate=func.now()
+    )
